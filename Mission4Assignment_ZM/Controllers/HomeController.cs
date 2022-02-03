1c0fbe4afe59b89b697f9dfbe88143e5336f2ef8
Zac McMillan <84478817+zacmcmillan29@users.noreply.github.com>
2022-02-02 20:49:12 -0700

    first upload

---
+++ Mission4Assignment_ZM/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission4Assignment_ZM.Models;
+
+namespace Mission4Assignment_ZM.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        //define scope 
+        private MovieAppContext blahContext { get; set; }
+
+        //controller
+        public HomeController(ILogger<HomeController> logger, MovieAppContext someName)
+        {
+            _logger = logger;
+            blahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddMovie (MovieAdd ma)
+        {
+            blahContext.Add(ma);
+            blahContext.SaveChanges();
+            return View("Confirmation", ma);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
